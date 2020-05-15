33a323337c41e9a710e37ee327345d1a91f4a481
Joann Pan <joannlpan@gmail.com>
2020-05-15 19:06:36 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -20,6 +20,18 @@ class Doctor
     Appointment.new(date, patient, self)
   end
 
+  def appointments
+    Appointment.all.select do |appointment|
+      appointment.doctor == self
+    end
+  end
+
+  def patients
+    appointments.map do |appointment|
+      appointment.patients
+    end
+  end
+
 
 
 
