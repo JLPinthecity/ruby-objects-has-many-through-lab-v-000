5fcaecd988c5c721a087ee71e538fda3eaf7dcc7
Joann Pan <joannlpan@gmail.com>
2020-05-15 17:59:41 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,21 @@
+class Artist
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    save
+  end
+
+  def self.all
+    @@all
+  end
+
+  def songs
+    
+  end
+
+
+
+end
